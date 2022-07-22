99f431c4a834fedf0c51c0b62aa2a38949f027b1
Michael Bond <codemonkey8510@icloud.com>
2022-07-22 16:59:02 -0400

    Add project files.

---
+++ AspNetWithReact/ApiEndpoints/WeatherApiEndpoints.cs
@@ -0,0 +1,22 @@
+﻿namespace AspNetWithReact.ApiEndpoints;
+
+public static class WeatherApiEndpoints
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static IEndpointRouteBuilder MapWeatherApiEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("WeatherForecast", Get);
+        return app;
+    }
+
+    private static IResult Get() => Results.Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    (
+        DateTime.Now.AddDays(index),
+        Random.Shared.Next(-20, 55),
+        Summaries[Random.Shared.Next(Summaries.Length)]
+    )).ToArray());
+}
---
+++ AspNetWithReact/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using System.Diagnostics;
+global using AspNetWithReact.ApiEndpoints;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.AspNetCore.Mvc.RazorPages;
